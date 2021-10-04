020cbf4ec9c7d679e8e528b63b2a8f5d3214eeab
eross <esr@eross.dev>
2021-10-03 21:44:24 -0400

    quick updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
 import preprocess from 'svelte-preprocess';
+import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess({
-        postcss: true
-    })],
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+		adapter: staticAdapter({
+			fallback: 'index.html',
+		}),
+	},
 };
 
 export default config;
